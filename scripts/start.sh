cb08016cd186ff1e244d0918a683f4e5213806ac
TheNeilist <theflyingcyclist@gmail.com>
2018-01-03 16:41:40 -0700

    update start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 BUILD_ID=pleaseDontKillMeMRS
-nohup java -jar /var/lib/jenkins/workspace/mrs/target/mrs-0.0.1.jar < /dev/null &
+nohup java -jar /var/lib/jenkins/workspace/mrs/target/mrs-0.0.1-SNAPSHOT.jar &> nohup.out
 MRS_PID=$!
 echo MRS_PID > /tmp/mrs-pid
 echo "started mrs PID $MRS_PID"
