3eb1d34efbf1131c3b964e43dc43dd60dbeecb24
TheNeilist <theflyingcyclist@gmail.com>
2018-01-03 01:31:14 -0700

    update scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 BUILD_ID=pleaseDontKillMeMRS
-nohup java -jar /var/lib/jenkins/workspace/everwas-api/target/everwas-api-0.0.1.jar server /var/lib/jenkins/workspace/everwas-api/everwas-api.yml > log.txt 2> errors.txt < /dev/null &
-EVERWAS_API_PID=$!
-echo $EVERWAS_API_PID > /tmp/everwas-api-pid
-echo "started everwas-api PID $EVERWAS_API_PID"
+nohup java -jar /var/lib/jenkins/workspace/mrs/target/mrs-0.0.1.jar < /dev/null &
+MRS_PID=$!
+echo MRS_PID > /tmp/mrs-pid
+echo "started mrs PID $MRS_PID"
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if [ -a /tmp/everwas-api-pid ]
+if [ -a /tmp/mrs-pid ]
         then
-                EVERWAS_API_PID=$(cat /tmp/everwas-api-pid)
-		echo "Killing process $EVERWAS_API_PID"
-                kill -9 $EVERWAS_API_PID
-                rm -f /tmp/everwas-api-pid
+                MRS_PID=$(cat /tmp/mrs-pid)
+		echo "Killing process MRS_PID"
+                kill -9 $MRS_PID
+                rm -f /tmp/mrs-pid
 	else
-		echo "pid file does not exist"
+		echo "mrs pid file does not exist"
 fi
