898088149394a6a5ceb0b3f4fc8b6ed0a8652f63
TheNeilist <theflyingcyclist@gmail.com>
2018-01-04 00:36:41 -0700

    update templates root

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 BUILD_ID=pleaseDontKillMeMRS
-nohup java -jar /var/lib/jenkins/workspace/mrs/target/mrs-0.0.1-SNAPSHOT.jar &> nohup.out
+nohup java -jar /var/lib/jenkins/workspace/mrs/target/mrs-0.0.1-SNAPSHOT.jar > nohup.out 2>&1 &
 MRS_PID=$!
 echo MRS_PID > /tmp/mrs-pid
 echo "started mrs PID $MRS_PID"
